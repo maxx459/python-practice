22073be43378ad771df99c6665521b81c2856859
maxx459 <22bq5a4202@vvit.net>
2026-02-28 23:42:50 +0530

    update

--- helloflask.py
+++ helloflask.py
@@ -5,3 +5,5 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+app.run() #we can also do like this to start instead of the "$flask --app helloflask run"
